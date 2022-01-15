95579dc68c990ba3bf5f22395613fc182d688cbe
NerVod <benjamin.jeannerot@gmail.com>
2022-01-15 17:25:02 +0100

    express bdd

--- 6-BDD/index.js
+++ 6-BDD/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { MongoClient } from 'mongodb'
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
@@ -12,29 +16,59 @@ const collName = 'users';
 
 // const client = MongoClient.connect();
 
+app.set("view engine", "pug");
 
 app.get('/mongo', (req, res) => {
 
-    client((err, client) => {
+    client.connect((err, client) => {
         if(err) {
             console.log(err)
         } else {
             const db = client.db(dbName);
             const collection = db.collection(collName);
 
-            collection.findOne({}, (err, result) => {
+            console.log('db : ', db.namespace[0]),
+            console.log('collection : ', collection.namespace[1]),
+
+
+            // collection.findOne({}, (err, result) => {
+            //     if(err) {
+            //         res.send(err);
+            //     } else {
+            //         const name = result.name;
+            //         const age = result.age;
+            //         const address = result.address;
+
+            //         res.send (`Bonjour ${name} ! Vous avez ${age} ans et vous habitez la ville de ${address.City}`);
+            //         console.log(res)
+
+            //         client.close();
+            //     };
+            // })
+            collection.find({}).toArray( (err, result) => {
                 if(err) {
                     res.send(err);
                 } else {
-                    const name = result.name;
-                    const age = result.age;
-                    const address = result.address;
-
-                    res.send (`Bonjour ${name} ! Vous avez ${age} ans et vous habitez la ville de ${address.city}`);
-
+                    const donnees = result
+                    console.log(donnees)
+                    const user1 = donnees[0];
+                    const user2 = donnees[1];
+                    const user3 = donnees[2];
+                    
+                    res.render('index.pug', {
+                        db: `${db}`,
+                        collection: `${collection}`,
+                        user1: `${user1}`,
+                        user2: `${user2}`,
+                        user3: `${user3}`
+                    });
+                    
                     client.close();
                 };
+                
             })
+            
+            
 
         }
     })
